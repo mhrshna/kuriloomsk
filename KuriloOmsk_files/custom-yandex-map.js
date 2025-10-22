724db9cc2a8fddd9aabaad70c1fc2b321f36bfa4
mhrshna <econom_and_ip@mail.ru>
2025-10-22 20:48:20 +0600

    2ond ver (major fix)

--- KuriloOmsk_files/custom-yandex-map.js
+++ KuriloOmsk_files/custom-yandex-map.js
@@ -1,14 +1,3 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
 
 window.onload = function() {
 	var myMap;
@@ -34,7 +23,7 @@ window.onload = function() {
 			searchControlProvider: 'yandex#search'
 		});
 
-		myMap.behaviors.disable('scrollZoom');
+		myMap.behaviors.enable(['drag', 'scrollZoom']);
 		
 		var objectManager = new ymaps.ObjectManager({
 			clusterize: true,
@@ -111,15 +100,7 @@ window.onload = function() {
 	}
 };
 
-}
-/*
-     FILE ARCHIVED ON 15:50:52 Apr 01, 2022 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 12:02:50 Oct 16, 2025.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
 
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
 /*
 playback timings (ms):
   captures_list: 0.626
